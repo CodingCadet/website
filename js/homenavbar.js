7aaf30774da6ed941b2225939b2ee798f14d88c6
Dennis Park <dennisinchoonpark@gmail.com>
2016-10-04 21:08:30 -0700

    removed unnecessary js, added white logo to landing navbar

--- js/homenavbar.js
+++ js/homenavbar.js
@@ -1,4 +1,4 @@
-;(function () {
+(function () {
 	'use strict';
 
 	// Window Scroll
@@ -7,23 +7,25 @@
 
 		$(window).scroll(function(event){
 
-		   	var header = $('#cc-header'),
-				scrlTop = $(this).scrollTop();
+			var header = $('#cc-header'),
+			headerImage = $('.cc-logo'),
+			scrlTop = $(this).scrollTop();
 
 			if ( scrlTop > 500 && scrlTop <= 2000 ) {
+				headerImage.attr('src', 'images/logo.png');
 				header.addClass('navbar-fixed-top cc-animated slideInDown');
 			} else if ( scrlTop <= 500) {
+				headerImage.attr('src', 'images/logo-white.png');
 				if ( header.hasClass('navbar-fixed-top') ) {
 					header.addClass('navbar-fixed-top cc-animated slideOutUp');
 					setTimeout(function(){
 						header.removeClass('navbar-fixed-top cc-animated slideInDown slideOutUp');
 					}, 100 );
 				}
-			} 
-			
+			}
 		});
 	};
-    $(function(){
-        windowScroll();
-    })
+	$(function(){
+		windowScroll();
+	})
 }());
